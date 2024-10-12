2d10dee7ad959efd293763fadc99fcfb24a4f5aa
abdullashaikh <abdullashaikh36319@gmail.com>
2024-10-13 00:39:57 +0530

    12 as All Design Part Done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,18 @@
-
+import logo from "../assets/AbdullaShaikhLogo.webp"
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa"
+import { FaSquareXTwitter } from "react-icons/fa6"
 const Navbar = () => {
     return (
-        <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
-           
+        <nav className=" mb-20 flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+                <img src={logo} alt="Logo"  className="mx-2 w-20 rounded-2xl"/>
+            </div>
+            <div className="m-8 flex items-center justify-center gap-4  text-2xl">
+                <FaLinkedin />
+                <FaGithub />
+                <FaSquareXTwitter />
+                <FaInstagram />
+            </div>
         </nav>
     )
 }
